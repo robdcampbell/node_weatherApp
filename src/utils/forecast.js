7b7b4d2eb865d331e7080601ba0d0129a25d42bd
Rob Campbell <rcampbell1@gal-057049.local>
2019-03-26 16:27:18 -0400

    Add temperatureHigh and temperatureLow to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude,longitude,callback)=> {
           callback('Unable to find location', undefined)
       } else {
           const current = body.currently;
-          callback(undefined, `${body.daily.data[0].summary} It is currently ${current.temperature} degrees out. There is a ${current.precipProbability}% chance of rain.`)
+          callback(undefined, `${body.daily.data[0].summary} It is currently ${current.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh}, and the low today is ${body.daily.data[0].temperatureLow} There is a ${current.precipProbability}% chance of rain.`)
       }
   })
 }
